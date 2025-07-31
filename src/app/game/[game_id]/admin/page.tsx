9fbdbfc642c248474d5038aec59d37a54befee87
Pavel Tsiukhtsiayeu <paveltyk@gmail.com>
2025-07-30 22:23:23 -0400

    Better UI

--- src/app/game/[game_id]/admin/page.tsx
+++ src/app/game/[game_id]/admin/page.tsx
@@ -19,6 +19,11 @@ import { QRCode } from "@/components/shared-assets/qr-code";
 import useChannel from "@/hooks/use-channel";
 import type { GameType } from "@/hooks/use-channel";
 
+// TODO: Refactor the screen for better UX;
+// 1. Setup big screen with this URL or share this link with players so they can join the game. CTA: Start game
+// 2. Track game progress. CTA: Next
+// 3. Leader board. Game over. CTA: New game
+
 const badgeColors = {
     true: "success",
     false: "error",
@@ -37,6 +42,14 @@ const TeamGuessBadge = ({ team, guess }) => {
     );
 };
 
+const ActiveIndicator = () => {
+    return (
+        <span className="relative flex size-2">
+            <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-brand-400 opacity-75"></span>
+            <span className="relative inline-flex size-2 rounded-full bg-brand-500"></span>
+        </span>
+    );
+};
 const QuestionsTable = ({ game, onNextQuestion, onGameStart, onNewGame }) => {
     const currentQuestion = game.currentQuestion;
     const questions = game.questions;
@@ -92,22 +105,52 @@ const QuestionsTable = ({ game, onNextQuestion, onGameStart, onNewGame }) => {
             />
             <Table aria-label="Questions and answers">
                 <Table.Header>
+                    <Table.Head id="Active" isRowHeader className="w-3 pr-1" />
                     <Table.Head id="Question" label="Question" isRowHeader />
                     <Table.Head id="Team Answer" label="Team Answer" isRowHeader />
                 </Table.Header>
 
                 <Table.Body items={questions}>
-                    {(item: { id: string; question: string; answer: string }) => (
-                        <Table.Row id={item.id} className={item.id === game.currentQuestion?.id ? "" : "opacity-50"}>
-                            <Table.Cell>
-                                <div>
-                                    <p className="text-primary">Q: {item.question}</p>
-                                    <p className="text-tertiary">A: {item.answer}</p>
-                                </div>
-                            </Table.Cell>
-                            <Table.Cell>
-                                <TeamGuesses teamGuesses={teamGuesses} teams={teams} item={item} />
-                            </Table.Cell>
+                    {(item: { id: string; question: string; answer: string }) => {
+                        const isActiveItem = item.id === game.currentQuestion?.id;
+                        return (
+                            <Table.Row id={item.id} className={!game.currentQuestion || isActiveItem ? "" : "opacity-50"}>
+                                <Table.Cell className="w-3 pr-1">
+                                    <div className="w-3">{isActiveItem && <ActiveIndicator />}</div>
+                                </Table.Cell>
+                                <Table.Cell>
+                                    <div>
+                                        <p className="text-primary">Q: {item.question}</p>
+                                        <p className="text-tertiary">A: {item.answer}</p>
+                                    </div>
+                                </Table.Cell>
+                                <Table.Cell>
+                                    <TeamGuesses teamGuesses={teamGuesses} teams={teams} item={item} />
+                                </Table.Cell>
+                            </Table.Row>
+                        );
+                    }}
+                </Table.Body>
+            </Table>
+        </TableCard.Root>
+    );
+};
+
+const Leaderboard = ({ items }) => {
+    return (
+        <TableCard.Root>
+            <TableCard.Header title="Leaderboard" badge={`${items.length} team${items.length === 1 ? "" : "s"}`} />
+            <Table aria-label="Questions and answers">
+                <Table.Header>
+                    <Table.Head id="Team" label="Team" isRowHeader />
+                    <Table.Head id="Score" label="Score" isRowHeader />
+                </Table.Header>
+
+                <Table.Body items={items}>
+                    {(item: { team: string; score: number }) => (
+                        <Table.Row id={item.team}>
+                            <Table.Cell>{item.team}</Table.Cell>
+                            <Table.Cell>{item.score}</Table.Cell>
                         </Table.Row>
                     )}
                 </Table.Body>
@@ -155,29 +198,30 @@ const GameAdminPage = () => {
                 <h1 className="mb-8 border-b-1 border-secondary pb-2 text-display-sm font-semibold text-primary">Admin panel for game "{params.game_id}"</h1>
 
                 <div className="mb-8">{game && <QuestionsTable game={game} onNextQuestion={nextQuestion} onGameStart={startGame} onNewGame={newGame} />}</div>
+                <div className="mb-8">{game && <Leaderboard items={game?.leaderboard} />}</div>
 
                 <div className="flex flex-row gap-4">
-                    <div className="max-w-md rounded-lg border border-secondary">
+                    <div className="max-w-md overflow-hidden rounded-lg border border-secondary">
                         <div className="border-b-1 border-primary p-4 text-lg font-semibold">Join URL</div>
                         <div className="p-4">
-                            <QRCode value={lobbyUrl} size="2xl" className="m-auto w-min" />
+                            <QRCode value={lobbyUrl} size="2xl" className="m-auto mb-4 w-min" />
                             <a href={lobbyUrl} target="blank" className="text-brand-700">
                                 {lobbyUrl}
                             </a>
                         </div>
-                        <div className="border-t-1 border-primary bg-gray-50 p-4 text-secondary">
+                        <div className="border-t-1 border-primary bg-secondary p-4 text-secondary">
                             Share this URL with the players so that they can join the game. That is where players will answer the questions.
                         </div>
                     </div>
-                    <div className="max-w-md rounded-lg border border-secondary">
+                    <div className="max-w-md overflow-hidden rounded-lg border border-secondary">
                         <div className="border-b-1 border-primary p-4 text-lg font-semibold">Screen URL</div>
                         <div className="p-4">
-                            <QRCode value={screenUrl} size="2xl" className="m-auto w-min" />
+                            <QRCode value={screenUrl} size="2xl" className="m-auto mb-4 w-min" />
                             <a href={screenUrl} target="blank" className="text-brand-700">
                                 {screenUrl}
                             </a>
                         </div>
-                        <div className="border-t-1 border-primary bg-gray-50 p-4 text-secondary">
+                        <div className="border-t-1 border-primary bg-secondary p-4 text-secondary">
                             Use this URL for your big screen or projector. That is where players will see the current question and game progress.
                         </div>
                     </div>
