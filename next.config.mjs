fc8e35807a83f6c306aa1ab608e9edf2f3b3ec83
Pavel Tsiukhtsiayeu <paveltyk@gmail.com>
2025-08-16 00:03:58 -0400

    Root redirect

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,15 @@ const nextConfig = {
     experimental: {
         optimizePackageImports: ["@untitledui/icons"],
     },
+    async redirects() {
+        return [
+            {
+                source: "/",
+                destination: "/game/new",
+                permanent: false,
+            },
+        ];
+    },
 };
 
 export default nextConfig;
